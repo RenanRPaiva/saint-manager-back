6e654425042a2ed2be3740e2a46b95f57a49dddc
Renan Paiva <renan_13rj@yahoo.com.br>
2022-11-01 22:28:53 -0300

    Implementa getAll

--- apis/Users/controllers/UsersController.js
+++ apis/Users/controllers/UsersController.js
@@ -1,7 +1,17 @@
+const database = require("../../../dbConfig/db/models");
 class UsersController {
     static async getAll(req, res){
-        return res.send("ClassContoler");
-    };
+        try {
+            const allUsers = await database.Users.findAll();
+            return res.status(200).send(allUsers); 
+        } catch (error) {
+            return res.status(500).send(error.message);
+        }
+    }
+
+    // static async getOne(req, res){
+    //     const 
+    // };
 };
 
 module.exports = UsersController;
