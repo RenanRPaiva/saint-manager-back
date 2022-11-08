c4245064b4223567854ab24c2bcef3e849987297
Renan Paiva <renan_13rj@yahoo.com.br>
2022-11-08 15:27:50 -0300

    Implementa get buscar somente 1 evento

--- apis/Eventos/controllers/EventosController.js
+++ apis/Eventos/controllers/EventosController.js
@@ -4,9 +4,23 @@ class EventoController {
     static async getAllEventos(req, res){
         try {
             const eventosAll = await database.Eventos.findAll();
-            return res.status(200).json(eventosAll);
+            return res.status(200).send(eventosAll);
         } catch (error) {
-            return res.status(500).json(error.message);            
+            return res.status(500).send(error.message);            
+        }
+    };
+
+    static async getOneEvento(req, res){
+        const { evento_id } = req.params;
+        try {
+            const oneEvento = await database.Eventos.findOne({ 
+                where: {
+                    id: Number(evento_id)
+                }
+             });
+             return res.status(200).send(oneEvento);
+        } catch (error) {
+            return res.status(500).send(error.message);               
         }
     };
  };
--- apis/Eventos/routes.js
+++ apis/Eventos/routes.js
@@ -3,5 +3,6 @@ const route = Router();
 const EventoController = require('./controllers/EventosController');
 
 route.get('/eventos', EventoController.getAllEventos);
+route.get('/evento/:evento_id', EventoController.getOneEvento);
 
 module.exports = route;
