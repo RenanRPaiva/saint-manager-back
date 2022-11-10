321162561707876a701b0899eab198620591015d
Renan Paiva <renan_13rj@yahoo.com.br>
2022-11-09 21:11:31 -0300

    Implementa rota de delete inscrito

--- apis/Inscritos/routes.js
+++ apis/Inscritos/routes.js
@@ -5,5 +5,6 @@ const route = Router();
 route.get('/user/:user_id/inscritos/:inscrito_id', InscritoController.getInscrito);
 route.put('/inscritos/:inscrito_id/user/:user_id', InscritoController.editInscrito);
 route.post('/inscritos/:user_id/user', InscritoController.createInscrito);
+route.delete('/inscritos/:inscrito_id', InscritoController.deleteInscrito);
 
 module.exports = route;
